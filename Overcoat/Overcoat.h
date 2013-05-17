f2e30a3a7252a847fdf2efed0edcaed61f7d985b
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2013-05-17 14:52:04 +0200

    Add Mac OS X target

---
+++ Overcoat/Overcoat.h
@@ -0,0 +1,36 @@
+// Overcoat.h
+//
+// Copyright (c) 2013 Guillermo Gonzalez
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef _OVERCOAT_H
+#define _OVERCOAT_H
+
+#import "OVCQuery.h"
+#import "OVCMultipart.h"
+#import "OVCClient.h"
+#import "OVCRequestOperation.h"
+#import "NSDictionary+Overcoat.h"
+
+#if ((__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_6_0) || (__MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_10_8))
+    #import "OVCSocialClient.h"
+#endif
+
+#endif /* _OVERCOAT_H */
