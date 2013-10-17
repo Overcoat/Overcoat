63d5c60ffbf9871f6c450dccee0be27d6bfbd3b5
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2013-10-17 14:18:06 +0200

    Fix ACAccount authentication for multipart requests.

--- Overcoat/OVCSocialRequestSerializer.m
+++ Overcoat/OVCSocialRequestSerializer.m
@@ -22,6 +22,84 @@
 
 #import "OVCSocialRequestSerializer.h"
 
+#pragma mark -
+
+@interface OVCMultipartFormData : NSObject <AFMultipartFormData>
+
+@property (strong, nonatomic, readonly) SLRequest *socialRequest;
+
+- (id)initWithSocialRequest:(SLRequest *)request;
+
+@end
+
+#pragma mark -
+
+@implementation OVCMultipartFormData
+
+- (id)initWithSocialRequest:(SLRequest *)request {
+    if (self = [super init]) {
+        _socialRequest = request;
+    }
+    return self;
+}
+
+- (BOOL)appendPartWithFileURL:(NSURL *)fileURL
+                         name:(NSString *)name
+                        error:(NSError * __autoreleasing *)error
+{
+    NSAssert(NO, @"OVCMultipartFormData: unsupported method");
+    return NO;
+}
+
+- (BOOL)appendPartWithFileURL:(NSURL *)fileURL
+                         name:(NSString *)name
+                     fileName:(NSString *)fileName
+                     mimeType:(NSString *)mimeType
+                        error:(NSError * __autoreleasing *)error
+{
+    NSAssert(NO, @"OVCMultipartFormData: unsupported method");
+    return NO;
+}
+
+- (void)appendPartWithInputStream:(NSInputStream *)inputStream
+                             name:(NSString *)name
+                         fileName:(NSString *)fileName
+                           length:(int64_t)length
+                         mimeType:(NSString *)mimeType
+{
+    NSAssert(NO, @"OVCMultipartFormData: unsupported method");
+}
+
+- (void)appendPartWithFileData:(NSData *)data
+                          name:(NSString *)name
+                      fileName:(NSString *)fileName
+                      mimeType:(NSString *)mimeType
+{
+    [self.socialRequest addMultipartData:data withName:name type:mimeType filename:fileName];
+}
+
+- (void)appendPartWithFormData:(NSData *)data
+                          name:(NSString *)name
+{
+    [self.socialRequest addMultipartData:data withName:name type:nil filename:nil];
+}
+
+- (void)appendPartWithHeaders:(NSDictionary *)headers
+                         body:(NSData *)body
+{
+    NSAssert(NO, @"OVCMultipartFormData: unsupported method");
+}
+
+- (void)throttleBandwidthWithPacketSize:(NSUInteger)numberOfBytes
+                                  delay:(NSTimeInterval)delay
+{
+    NSAssert(NO, @"OVCMultipartFormData: unsupported method");
+}
+
+@end
+
+#pragma mark -
+
 @implementation OVCSocialRequestSerializer
 
 + (instancetype)serializerWithAccount:(ACAccount *)account {
@@ -38,27 +116,34 @@
     return serializer;
 }
 
-#pragma mark - AFURLRequestSerialization
+#pragma mark - AFHTTPRequestSerializer
 
-- (NSURLRequest *)requestBySerializingRequest:(NSURLRequest *)request
-                               withParameters:(NSDictionary *)parameters
-                                        error:(NSError *__autoreleasing *)error
+- (NSMutableURLRequest *)requestWithMethod:(NSString *)method
+                                 URLString:(NSString *)URLString
+                                parameters:(NSDictionary *)parameters
 {
-    NSParameterAssert(request);
+    SLRequest *socialRequest = [self socialRequestWithMethod:method
+                                                   URLString:URLString
+                                                  parameters:parameters];
     
-    NSString *serviceType = [self requestServiceType];
-    NSAssert(serviceType, @"*** No service type found!");
+    return [[socialRequest preparedURLRequest] mutableCopy];
+}
+
+- (NSMutableURLRequest *)multipartFormRequestWithMethod:(NSString *)method
+                                              URLString:(NSString *)URLString
+                                             parameters:(NSDictionary *)parameters
+                              constructingBodyWithBlock:(void (^)(id <AFMultipartFormData> formData))block
+{
+    SLRequest *socialRequest = [self socialRequestWithMethod:method
+                                                   URLString:URLString
+                                                  parameters:parameters];
     
-    SLRequestMethod method = [self.class socialRequestMethodForMethod:request.HTTPMethod];
-    SLRequest *socialRequest = [SLRequest requestForServiceType:serviceType
-                                                  requestMethod:method
-                                                            URL:request.URL
-                                                     parameters:nil];
-    socialRequest.account = self.account;
+    if (block) {
+        OVCMultipartFormData *formData = [[OVCMultipartFormData alloc] initWithSocialRequest:socialRequest];
+        block(formData);
+    }
     
-    return [super requestBySerializingRequest:socialRequest.preparedURLRequest
-                               withParameters:parameters
-                                        error:error];
+    return [[socialRequest preparedURLRequest] mutableCopy];
 }
 
 #pragma mark - NSCoding
@@ -101,6 +186,26 @@
     }
     
     return self.serviceType;
+}
+
+- (SLRequest *)socialRequestWithMethod:(NSString *)method
+                             URLString:(NSString *)URLString
+                            parameters:(NSDictionary *)parameters
+{
+    NSParameterAssert(method);
+    NSParameterAssert(URLString);
+    
+    NSString *serviceType = [self requestServiceType];
+    NSAssert(serviceType, @"*** No service type found!");
+    
+    SLRequestMethod requestMethod = [self.class socialRequestMethodForMethod:method];
+    SLRequest *socialRequest = [SLRequest requestForServiceType:serviceType
+                                                  requestMethod:requestMethod
+                                                            URL:[NSURL URLWithString:URLString]
+                                                     parameters:parameters];
+    socialRequest.account = self.account;
+    
+    return socialRequest;
 }
 
 + (NSString *)serviceTypeForAccountTypeIdentifier:(NSString *)accountTypeIdentifier {
