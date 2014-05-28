b1f05811a99226a4ac805f1ec57fa95fab99662b
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2014-05-28 14:42:50 +0200

    Add timeline view controller

---
+++ Examples/TwitterTimeline/TwitterTimeline/TimelineViewController.h
@@ -0,0 +1,13 @@
+//
+//  TimelineViewController.h
+//  TwitterTimeline
+//
+//  Created by guille on 27/05/14.
+//  Copyright (c) 2014 Guillermo Gonzalez. All rights reserved.
+//
+
+#import "TGRFetchedResultsTableViewController.h"
+
+@interface TimelineViewController : TGRFetchedResultsTableViewController
+
+@end
---
+++ Examples/TwitterTimeline/TwitterTimeline/TimelineViewController.m
@@ -0,0 +1,48 @@
+//
+//  TimelineViewController.m
+//  TwitterTimeline
+//
+//  Created by guille on 27/05/14.
+//  Copyright (c) 2014 Guillermo Gonzalez. All rights reserved.
+//
+
+#import "TimelineViewController.h"
+
+@interface TimelineViewController ()
+
+@end
+
+@implementation TimelineViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    if (self) {
+        // Custom initialization
+    }
+
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+   #pragma mark - Navigation
+
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+   {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+   }
+ */
+
+@end
