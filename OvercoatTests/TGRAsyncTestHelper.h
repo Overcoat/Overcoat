addca646fbfc88347d65ed0c30f32ca74f7f8c18
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2014-05-29 08:43:11 +0200

    Attempt to fix intermittently failing tests

--- OvercoatTests/TGRAsyncTestHelper.h
+++ OvercoatTests/TGRAsyncTestHelper.h
@@ -6,8 +6,9 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <libkern/OSAtomic.h>
 
-#define TGR_RUNLOOP_INTERVAL 0.05
+#define TGR_RUNLOOP_INTERVAL 0.01
 #define TGR_TIMEOUT_INTERVAL 10.0
 #define TGR_RUNLOOP_COUNT TGR_TIMEOUT_INTERVAL / TGR_RUNLOOP_INTERVAL
 
@@ -21,6 +22,7 @@ while (!(a1) && __runLoopCount < TGR_RUNLOOP_COUNT) { \
     NSDate* date = [NSDate dateWithTimeIntervalSinceNow:TGR_RUNLOOP_INTERVAL]; \
     [NSRunLoop.currentRunLoop runUntilDate:date]; \
     __runLoopCount++; \
+    OSMemoryBarrier(); \
 } \
 if (__runLoopCount >= TGR_RUNLOOP_COUNT) { \
     XCTFail(format); \
