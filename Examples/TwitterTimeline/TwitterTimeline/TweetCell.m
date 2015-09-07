77b56a1b4948b07c7db5e36196ada515e56d2e16
sodastsai <sodas2002@gmail.com>
2015-09-07 22:38:46 +0800

    Update Demo app

--- Examples/TwitterTimeline/TwitterTimeline/TweetCell.m
+++ Examples/TwitterTimeline/TwitterTimeline/TweetCell.m
@@ -60,7 +60,12 @@
     NSCalendar *calendar = [NSCalendar currentCalendar];
     NSDate *now = [NSDate date];
 
-    NSUInteger unitFlags = NSSecondCalendarUnit | NSMinuteCalendarUnit | NSHourCalendarUnit | NSDayCalendarUnit;
+#if ((defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000) || \
+    (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 101000))
+    NSCalendarUnit unitFlags = NSCalendarUnitSecond | NSCalendarUnitMinute | NSCalendarUnitHour | NSCalendarUnitDay;
+#else
+    NSCalendarUnit unitFlags = NSSecondCalendarUnit | NSMinuteCalendarUnit | NSHourCalendarUnit | NSDayCalendarUnit;
+#endif
     NSDateComponents *components = [calendar components:unitFlags
                                                fromDate:now
                                                  toDate:date
